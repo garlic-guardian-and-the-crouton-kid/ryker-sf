3ee9db3b167d5a82c295597ee3a0f9f83eaa5c43
jbolling <josephcbolling@gmail.com>
2018-03-14 14:24:53 -0700

    Fixing pointer bug

--- src/point_match.cc
+++ src/point_match.cc
@@ -1,25 +1,27 @@
 #include <opencv2/features2d.hpp>
 #include "point_match.h"
 #include <memory>
+#include <iostream>
 
 
 namespace ggck {
 namespace point_match {
  
 Mat GetPoints(Mat im1, Mat im2) {
-
-  // UNCERTAIN: does create() get called properly by make_unique?
-  cv::ORB* detector = cv::ORB::create();
-  cv::DescriptorMatcher* matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
+  cv::Ptr<cv::ORB> detector = cv::ORB::create();
+  cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
 
   Mat d1, d2;
   std::vector<cv::KeyPoint> kp1, kp2;
+  Mat out_image;
 
-  // UNCERTAIN: NULL for mask input?
-  detector->detectAndCompute(im1, NULL, kp1, d1);
+  // Get keypoints and descriptors for both images
+  detector->detectAndCompute(im1, cv::noArray(), kp1, d1);
+  detector->detectAndCompute(im2, cv::noArray(), kp2, d2);
 
-  Mat points;
-  return points;
+  cv::drawKeypoints(im1, kp1, out_image);
+
+  return out_image;
 }
 
 } //namespace point_match
